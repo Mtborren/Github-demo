50f4bcc072e0806d2e46f452142837aa0d091541
vcjronijohn <ronijohnveronica@gmail.com>
2020-01-29 14:22:46 -0500

    added to homework and played with more console math

--- java.js
+++ java.js
@@ -4,10 +4,20 @@ var sassyString
 irateInteger = 7
 sassyString = "this string is sassy"
 
-console.log(irateInteger * sassyString)
+console.log(irateInteger * sassyString);
 //for this answer I received NaN
 
-console.log(2 + true)
+console.log(2 + true);
 //for this answer I received 3.
 
-console.log(irateInteger + 15)
+console.log(irateInteger + 15);
+//just playing with the math a little.
+
+console.log(sassyString * sassyString);
+//trying to multiply strings together - ended with NaN
+
+console.log(sassyString + sassyString);
+//trying to add strings
+
+console.log(irateInteger + "7");
+//adding strings and integers together
