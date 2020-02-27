afc069b0a038ee865e0ba66a87ff696ff4b86c0d
vcjronijohn <ronijohnveronica@gmail.com>
2020-02-27 15:52:33 -0500

    added note

--- Homework/exerciseserver/server.js
+++ Homework/exerciseserver/server.js
@@ -3,10 +3,16 @@
 // Place h1 tags around "Hello, World!". Display this string using the web server as html.
 
 const http = require('http');
-// const fs = require('fs');
+const fs = require('fs');
+const path = require('path')
+
 // const fileName = 'path/to/file';
 // const stream = fs.createWriteStream(fileName);
 // const port = 8080;
+
+let filePath = '.' + request.url;
+
+//look at morgans repo titled solid parakeet to fix this code
 
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
