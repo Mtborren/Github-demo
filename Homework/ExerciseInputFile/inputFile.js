0aa801cf3e50f86c8cdf6cd662322219c2ec4251
vcjronijohn <ronijohnveronica@gmail.com>
2020-02-27 14:36:46 -0500

    asdf

--- Homework/ExerciseInputFile/inputFile.js
+++ Homework/ExerciseInputFile/inputFile.js
@@ -4,3 +4,37 @@
 // When a file has been created
 // When a file has been written to
 // When a file has been read 
+
+const fs = require('fs');
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question(`What's your favorite cartoon?\n`, (cartoon) => {
+    myEmitter.emit('create');
+    fs.appendFile('message.txt', cartoon, function(err){
+        if(err) throw err;
+        console.log(`Oh cool, I like ${cartoon} too!`)
+        myEmitter.emit('write');
+    });
+    readline.close();
+})
+
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+
+myEmitter.on('create', () => {
+    console.log("File Created.");
+})
+
+myEmitter.on('write', () => {
+    console.log("File Written.");
+})
+
+let rs = fs.createReadStream('message.txt');
+
+rs.on('open', () => {
+    console.log("The file has been read.")
+});
