7b780398163ef20284eea400a2b1a7d5ce7bc323
vcjronijohn <ronijohnveronica@gmail.com>
2020-03-02 12:24:16 -0500

    fixed server homework

--- Homework/exerciseserver/server.js
+++ Homework/exerciseserver/server.js
@@ -3,18 +3,10 @@
 // Place h1 tags around "Hello, World!". Display this string using the web server as html.
 
 const http = require('http');
-const fs = require('fs');
-//look at morgans repo titled solid parakeet to fix this code
-
 
 http.createServer(function(request, response) {
-    http.get('./Desktop\cbc8\Github-demo\Homework\ExerciseServer\server.js', (res) => {
-        let newHead = document.createElement("h1");
-        let newContent = document.createTextNode("Hello, World!")
-        newHead.appendChild(newContent);
-    })
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write("Hello, World!");
-    response.end();
+    response.write("<h1>It Finally Works!!</h1>");
+    response.end()
 }).listen(8080);
 
